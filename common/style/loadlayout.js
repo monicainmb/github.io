abfadeb9e747cfdd49be390d24d20d6dcaad0267
monicainmb <36257911+monicainmb@users.noreply.github.com>
2018-02-15 21:56:26 +0900

    20180215
    ヘッダーとかの共通部分をjsで出力

--- common/style/loadlayout.js
+++ common/style/loadlayout.js
@@ -1,5 +1,29 @@
-$(function(){
-	$("#footer").load("https://monicainmb.github.io/common/style/footer.html");
-	$("#header").load("https://monicainmb.github.io/common/style/header.html");
-	$("#linkmenu").load("https://monicainmb.github.io/common/style/linkmenu.html");
-});
+function header(){
+	var html = "";
+	
+	html += "<h1><U>monicaの管理室</U></h1>";
+	html += "<p>何か作ったものを載せるだけのHP</p>";
+
+	document.write(html);
+}
+
+function linkmenu(){
+	var html = "";
+	
+	html += "<ul>";
+	html += "	作ったもの";
+	html += "	<li>ゲーム</li>";
+	html += "	<li>ツール</li>";
+	html += "	<li>サンプル</li>";
+	html += "</ul>";
+
+	document.write(html);
+}
+
+function footer(){
+	var html = "";
+	
+	html += "<p>ふったー</p>";
+	
+	document.write(html);
+}
